e81357a7a74272afdcb20a48ab1199504a3d3767
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-05-05 15:48:38 -0300

    Entity User e REST funcionando

---
+++ src/main/java/com/mongodb/springboot2/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.mongodb.springboot2.resources;
+
+import com.mongodb.springboot2.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity <List<User>> findAll() {
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        List<User> list = new ArrayList<>();
+        list.addAll(Arrays.asList(maria, alex));
+        return ResponseEntity.ok().body(list);
+    }
+}
