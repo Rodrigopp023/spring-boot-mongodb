6bff43c8c936375c5171cb9829e8d2caa392e2a7
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-05-06 16:54:31 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/mongodb/springboot2/repository/PostRepository.java
@@ -0,0 +1,9 @@
+package com.mongodb.springboot2.repository;
+
+import com.mongodb.springboot2.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
--- src/main/java/com/mongodb/springboot2/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/mongodb/springboot2/resources/exception/ResourceExceptionHandler.java
@@ -4,8 +4,9 @@ import com.mongodb.springboot2.services.exception.ObjectNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
@@ -13,10 +14,9 @@ public class ResourceExceptionHandler {
     @ExceptionHandler(ObjectNotFoundException.class)
     public ResponseEntity<StandardError>
     objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
-
         HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardError err = new StandardError(System.currentTimeMillis(),
-                status.value(), "Not found", e.getMessage(), request.getRequestURI());
+        StandardError err = new StandardError(Instant.now(), status.value(),
+                "Not found", e.getMessage(), request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 }
