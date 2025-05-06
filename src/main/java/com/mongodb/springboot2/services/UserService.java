4c51fc46a46043c9b86e38383b61e40943812c4a
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-05-06 15:05:56 -0300

    Inserção de usuário com POST

--- src/main/java/com/mongodb/springboot2/services/UserService.java
+++ src/main/java/com/mongodb/springboot2/services/UserService.java
@@ -1,6 +1,7 @@
 package com.mongodb.springboot2.services;
 
 import com.mongodb.springboot2.domain.User;
+import com.mongodb.springboot2.dto.UserDTO;
 import com.mongodb.springboot2.repository.UserRepository;
 import com.mongodb.springboot2.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(()
                 -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
