3e3d96c7c7542d6587e3e98829e69df3e8663f15
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-05-07 13:51:38 -0300

    Referenciando os posts do usuário

--- src/main/java/com/mongodb/springboot2/config/Instantiation.java
+++ src/main/java/com/mongodb/springboot2/config/Instantiation.java
@@ -30,7 +30,7 @@ public class Instantiation implements CommandLineRunner {
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
-        
+
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
 
         Post post1 = new Post(null, sdf.parse("2018/03/21"), "Partiu viagem!",
@@ -39,5 +39,7 @@ public class Instantiation implements CommandLineRunner {
                 "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+        maria.getPosts().addAll(Arrays.asList(post1, post1));
+        userRepository.save(maria);
     }
 }
