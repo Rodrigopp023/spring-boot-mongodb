26c3ae67e2152ff35fe986bd29e7b4fc8c7ce873
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-05-07 15:11:46 -0300

    Consulta simples com query methods

--- src/main/java/com/mongodb/springboot2/resources/PostResource.java
+++ src/main/java/com/mongodb/springboot2/resources/PostResource.java
@@ -1,10 +1,13 @@
 package com.mongodb.springboot2.resources;
 
 import com.mongodb.springboot2.domain.*;
+import com.mongodb.springboot2.resources.util.URL;
 import com.mongodb.springboot2.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -17,5 +20,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text",
+            defaultValue = "") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 }
