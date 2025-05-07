47ea776854364357ca3e6cacd79c7d575284fc44
Rodrigo <rodrigo.paz.2310@gmail.com>
2025-05-07 15:24:56 -0300

    Consulta simples com @Query

--- src/main/java/com/mongodb/springboot2/repository/PostRepository.java
+++ src/main/java/com/mongodb/springboot2/repository/PostRepository.java
@@ -1,13 +1,16 @@
 package com.mongodb.springboot2.repository;
 
 import com.mongodb.springboot2.domain.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ title: { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
